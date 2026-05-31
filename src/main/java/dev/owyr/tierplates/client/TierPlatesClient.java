496b983a55ebbb2f3c09ea9dd5c9b274d3d18c0e
OwYr777 <OwYr777@users.noreply.github.com>
2026-05-31 12:42:06 +0200

    Add configurable settings keybind

--- src/main/java/dev/owyr/tierplates/client/TierPlatesClient.java
+++ src/main/java/dev/owyr/tierplates/client/TierPlatesClient.java
@@ -6,21 +6,51 @@ import dev.owyr.tierplates.client.screen.TierPlatesConfigScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public final class TierPlatesClient implements ClientModInitializer {
     private static TierPlatesConfig config;
+    private static KeyBinding openSettingsKey;
 
     @Override
     public void onInitializeClient() {
         config = ConfigManager.load();
+        registerKeybind();
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             dispatcher.register(ClientCommandManager.literal("tiersbyowyr7").executes(context -> {
-                MinecraftClient client = MinecraftClient.getInstance();
-                client.execute(() -> client.setScreen(new TierPlatesConfigScreen(client.currentScreen)));
+                openSettings();
                 return 1;
             }));
+        });
+    }
+
+    public static void openSettings() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        client.execute(() -> client.setScreen(new TierPlatesConfigScreen(client.currentScreen)));
+    }
+
+    private static void registerKeybind() {
+        KeyBinding.Category category = KeyBinding.Category.create(Identifier.of("tierplates", "tierplates"));
+        openSettingsKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.tierplates.open_settings",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_UNKNOWN,
+                category
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openSettingsKey.wasPressed()) {
+                if (client.currentScreen == null) {
+                    client.setScreen(new TierPlatesConfigScreen(null));
+                }
+            }
         });
     }
 
--- src/main/java/dev/owyr/tierplates/client/compat/TierPlatesModMenu.java
+++ src/main/java/dev/owyr/tierplates/client/compat/TierPlatesModMenu.java
@@ -2,11 +2,15 @@ package dev.owyr.tierplates.client.compat;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import dev.owyr.tierplates.client.TierPlatesClient;
 import dev.owyr.tierplates.client.screen.TierPlatesConfigScreen;
 
 public final class TierPlatesModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return TierPlatesConfigScreen::new;
+        return parent -> {
+            TierPlatesClient.config();
+            return new TierPlatesConfigScreen(parent);
+        };
     }
 }
